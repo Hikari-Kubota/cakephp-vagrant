fba7c33ac0edeacbd22100e573061b00d340fa8b
窪田 光 <h-kubota@njcadmin-no-MacBook-Pro-2.local>
2016-03-28 18:27:47 +0900

    Change salt and seed

--- webroot/cake_list/app/Config/core.php
+++ webroot/cake_list/app/Config/core.php
@@ -225,12 +225,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'KihxWfDfYZiYchsNRBUSqPzNmiiF77BHfTCC5ce9');	/* Changed on 2016/03/28
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '242398586254327274788967426998');	/* Changed on 2016/03/28
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
